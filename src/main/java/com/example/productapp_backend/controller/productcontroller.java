546119a351c62c249a3be98a0539c78395659a19
Sayanaann <alexsayana@gmail.com>
2022-12-05 20:21:19 +0530

    add

--- src/main/java/com/example/productapp_backend/controller/productcontroller.java
+++ src/main/java/com/example/productapp_backend/controller/productcontroller.java
@@ -1,6 +1,7 @@
 package com.example.productapp_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class productcontroller {
     @GetMapping("/")
     public String Homepage(){
         return "welcome to product app";
+
+    }
+    @PostMapping("/add")
+    public String Productadd(){
+        return "Add the product ";
 
     }
 }
